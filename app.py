fd351f5b42f9718d86ff818914017b89b23b94e3
priyo-02 <107844833+priyo-02@users.noreply.github.com>
2024-02-05 18:30:36 +0000

    app

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import pickle
+import pandas as pd
+
+movies_dict = pickle.load(open('movie_dict.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
 
 st.title('Movie Recomender System')
 
 option = st.selectbox(
-    
+    movies['title'].values
 )
